3e585d337a76ae67bec5b1ddaa86920fd6de82e1
Estanislau Trepat <estanis@etrepat.com>
2010-11-05 16:09:04 +0100

    forgot a fu**** debug.console statement

--- javascripts/main.js
+++ javascripts/main.js
@@ -10,37 +10,37 @@ Site.BackgroundLoader = Class.create({
       element: 'background',
       url: 'http://www.nasa.gov/images/content/473675main_473530main_f_211_193_171-orig_full_full.jpg'
     };
-    
+
     this.element = null;
-    
+
     this.loader = new Image();
     this.loader.observe('load', this.onImageLoaded.bind(this));
-    
+
     this.load(options);
   },
-  
+
   load: function(options) {
     if ( typeof options == 'string' )
       Object.extend(this.options, {url: options});
     else
       Object.extend(this.options, options || {});
-      
+
     if ( !this.options.url ) return;
     if ( this.options.url.strip() == '' ) return;
 
     this.element = $(this.options.element);
     if ( !this.element ) return;
-    
+
     this.loader.src = this.options.url;
   },
-  
+
   onImageLoaded: function(e) {
     this.element.setStyle({
       background: 'url(' + this.loader.src + ')',
       backgroundRepeat: 'no-repeat',
-      backgroundPosition: 'center center'          
+      backgroundPosition: 'center center'
     });
-    this.element.appear(1);    
+    this.element.appear(1);
   }
 });
 
@@ -53,12 +53,11 @@ Object.extend(Site.BackgroundLoader, {
       url: fromUrl
     });
   },
-  
+
   fromJSONP: function(jsonp) {
     if ( typeof jsonp['error'] === 'undefined' ) {
       if ( typeof jsonp['src'] !== 'undefined' ) {
         Site.BackgroundInfo = jsonp;
-        console.debug(Site.BackgroundInfo);
         return Site.BackgroundLoader.load('background', jsonp['src']);
       }
     }
@@ -71,45 +70,46 @@ Site.LazyScriptLoader = Class.create({
       type: 'text/javascript',
       async: true,
       position: 'last',
-      onLoad: Prototype.emptyFunction      
+      onLoad: Prototype.emptyFunction
     };
     Object.extend(opts, options || {});
-    
+
     var script = new Element('script', {
-      src: url, 
-      type: opts.type, 
+      src: url,
+      type: opts.type,
       async: opts.async
     });
     script.observe('load', opts.onLoad);
-    
+
     if ( opts.position == 'first' )
       $$('script').first().insert({before: script});
-    else 
+    else
       $$('script').last().insert({after: script});
   }
 });
 
 (function() {
   var loader = new Site.LazyScriptLoader();
-  
+
   document.observe('dom:loaded', function(e) {
     // lazily-load webfonts from google
     loader.load('http://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js', {
       position: 'first'
     });
-    
+
     // load NASA image of the day
     loader.load('http://nimod-jsonp.etrepat.com/?callback=Site.BackgroundLoader.fromJSONP');
-    
+
     // add nice hover effect to nav links
     $$('#header nav ul li a').each(function(anchor) {
       anchor.observe('mouseenter', function(e) {
         e.findElement().morph('background-color: #fff; color: #0af', .2);
       });
-      
+
       anchor.observe('mouseleave', function(e) {
         e.findElement().morph('background-color: #0af; color: #fff', .2);
       });
     });
-  });  
+  });
 })();
+
